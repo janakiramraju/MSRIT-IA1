1362a9eaec115c4ad81ac040b330ce37cd1cc034
janakiramraju <112241355+janakiramraju@users.noreply.github.com>
2022-12-22 09:42:49 +0530

    Add files via upload

---
+++ swap.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+int a=10,b=20,temp=0;
+printf("values before swaping are %d and %d",a,b);
+temp=a;
+a=b;
+b=temp;
+printf("values after swaping are %d and %d",a,b);
+}
